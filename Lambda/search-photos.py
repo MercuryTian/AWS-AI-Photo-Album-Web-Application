de2a9f37f7753576434093b2a839fae5a3a5c418
Zhengxi Tian <mercury0505@outlook.com>
2019-05-16 20:23:46 -0400

    Add files via upload

--- Lambda/search-photos.py
+++ Lambda/search-photos.py
@@ -3,6 +3,7 @@ import boto3
 import os
 import sys
 import uuid
+import time
 from botocore.vendored import requests
 
 ES_HOST = 'https://vpc-photos-rsjxyzqwdjlisyiem3w4iwldya.us-east-1.es.amazonaws.com'
@@ -21,6 +22,55 @@ def lambda_handler(event, context):
 	lex = boto3.client('lex-runtime')
 
 	query = event["queryStringParameters"]["q"]
+	
+	if query == "voiceSearch":
+		transcribe = boto3.client('transcribe')
+		job_name = time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()).replace(":", "-").replace(" ", "")
+		job_uri = "https://s3.amazonaws.com/cc3-voices/test.mp3"
+		transcribe.start_transcription_job(
+		    TranscriptionJobName=job_name,
+		    Media={'MediaFileUri': job_uri},
+		    MediaFormat='mp3',
+		    LanguageCode='en-US'
+		)
+		while True:
+		    status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+		    if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+		        break
+		    print("Not ready yet...")
+		    time.sleep(5)
+		print("Transcript URL: ", status)
+		transcriptURL = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
+		trans_text = requests.get(transcriptURL).json()
+		
+		print("Transcripts: ", trans_text)
+		print(trans_text["results"]['transcripts'][0]['transcript'])
+		
+		s3client = boto3.client('s3')
+		response = s3client.delete_object(
+		    Bucket='cc3-voices',
+		    Key='test.mp3'
+		)
+		query = trans_text["results"]['transcripts'][0]['transcript']
+		s3client.put_object(Body=query, Bucket='cc3-voices', Key='test.txt')
+		return {
+			'statusCode': 200,
+			'headers': {
+				"Access-Control-Allow-Origin": "*"
+			},
+			'body': "transcribe done"
+		}
+	
+	if query == "voiceResult":
+		s3client = boto3.client('s3')
+		data = s3client.get_object(Bucket='cc3-voices', Key='test.txt')
+		query = data.get('Body').read().decode('utf-8')
+		print("Voice query: ", query)
+		s3client.delete_object(
+			Bucket='cc3-voices',
+			Key='test.txt'
+		)
+	
 
 	lex_response = lex.post_text(
 		botName='PhotoAlbum',
